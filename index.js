1f2cb0e3b231ee136509ffedbb865bbaca2546a5
artiscruelty <danielfroestbf@gmail.com>
2022-04-05 17:07:17 -0300

    Index.js criado

---
+++ index.js
@@ -0,0 +1,10 @@
+const cliente1Nome = "Daniel";
+const cliente1Cpf = "1122233309";
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2Cpf = "22233344409";
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
