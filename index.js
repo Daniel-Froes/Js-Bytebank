fab3fbf71beb65a6943070cc4176897fb0643853
artiscruelty <danielfroestbf@gmail.com>
2022-04-05 19:47:29 -0300

    feature: criado modulos das classes e importadas, com as funções de sacar,depositar e transferir

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const cliente1Nome = "Daniel";
-const cliente1Cpf = "1122233309";
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente2Nome = "Alice";
-const cliente2Cpf = "22233344409";
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+cliente1.nome = "Daniel";
+cliente1.cpf = "1122233309";
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = "22233344409";
+
+const contaCorrenteDaniel = new ContaCorrente();
+
+contaCorrenteDaniel.agencia = 1001;
+contaCorrenteDaniel.cliente = cliente1;
+contaCorrenteDaniel.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+
+contaCorrenteDaniel.transferir(200, conta2)
+
+console.log(conta2)
+
 
